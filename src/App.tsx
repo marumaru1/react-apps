18e722b1ce80bc2d4aed64002cda23540d77a66e
Maria Makliuk <annamary_1995@mail.ru>
2022-10-06 02:07:55 +0300

    feat:full

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+
+import './style.scss'
+import IMAGE from './react.png'
+import LOGO from './logo.svg'
+
 export const App = () => {
-    return <h1>React Typescript Webpack Starter Template</h1>
+    return <>
+    <h1>React Typescript Webpack Starter Template</h1>
+    <img src={IMAGE} alt="React Logo" width="500" height="200"/>
+    <img src={LOGO} alt="React Logo" width="300" />
+    </>
 }
